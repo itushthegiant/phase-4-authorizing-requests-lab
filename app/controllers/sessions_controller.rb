3e56a427835e8414fbf4370dd9b3c816eb909e1e
etamar <etamar.levy@gmail.com>
2021-10-11 21:06:52 -0400

    commit tree 05ce538be9206c13e03376b202d70cfdbf6d441a

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,8 @@
 class SessionsController < ApplicationController
   
+  # skip_before_action :unauthorized, only: :create
+
+
   def create
     user = User.find_by(username: params[:username])
     session[:user_id] = user.id
